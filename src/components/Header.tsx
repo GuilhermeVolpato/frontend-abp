593a517583668e0654b74cda846e182e6b202b89
Guilherme Chaves Volpato <volpato2103@gmail.com>
2023-06-04 19:16:56 -0300

    feat: esboço tela de login e componente header do sistema

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Header() {
+  return (
+    <>
+      <header className="bg-red-600 h-20 max-w-full flex  items-center">
+        <h1 className="font-bold ml-60 text-4xl text-white">QuickDemand</h1>
+      </header>
+    </>
+  );
+}
